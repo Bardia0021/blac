6b99753e9ed856b4a150fe9687bac1cc128dff03
Bardia0021 <f8s4@yahoo.com>
2016-05-16 12:18:41 +0430

    Update sudoers.lua

--- plugins/sudoers.lua
+++ plugins/sudoers.lua
@@ -4,9 +4,8 @@ function run(msg, matches)
   return [[
 #Sudoers!
   
-  1.@aliebadi5🌐 #Developer ( use #adddeveloper command to add #AliEbadi in your Groups & SuperGroups. )
+  1.@Bardia021 #Developer ( use #adddeveloper command to add #AliEbadi in your Groups & SuperGroups. )
   
-   1.@AliE1_bot  manager   ( use #adddeveloper command to add #AliEbadi in your Groups & SuperGroups. )
 ]]
 end
 return {
