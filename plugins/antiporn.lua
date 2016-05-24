c8fdafe0680a9ce355070ffd8d58ecd41aaeb3c3
Bardia0021 <f8s4@yahoo.com>
2016-05-24 01:17:07 -0700

    Update antiporn.lua

--- plugins/antiporn.lua
+++ plugins/antiporn.lua
@@ -1,51 +1,37 @@
-
-do
+local function nuditycheck(msg, success, result)
+  if success then
+local file = 'data/nudity/'..string.sub(result, 38)
+os.rename(result, file)
+  local curl = 'curl -X POST "https://api.sightengine.com/1.0/nudity.json" -F "api_user=nil" -F "api_secret=nil" -F "photo=@'..file..'"'
+  local jcmd = io.popen(curl)
+  
+  local res = jcmd:read('*all')
+  local jdat = json:decode(res)
+if jdat.status then
+      if jdat.status == 'failure' then
+	     send_large_msg(get_receiver(msg), jdat.error_message, ok_cb, false)
+          elseif jdat.status == 'success' then
+             if jdat.nudity.result then
+	     send_large_msg(get_receiver(msg), 'این یک تصویر پورن است', ok_cb, false)
+         else
+         send_large_msg(get_receiver(msg), 'این یک تصویر سادست', ok_cb, false)
+         end
+  end
+end
+  else
+    print('Error downloading: '..msg.id)
+    send_large_msg(get_receiver(msg), 'Failed, please try again!', ok_cb, false)
+  end
+end
 local function pre_process(msg)
-if not is_momod(msg)  then
-if msg then
-  if mp4 == 'giphy.mp4' then
-    hash = 'mustafaip:'..msg.to.id
-    if redis:get(hash) then
-     delete_msg(msg.id, ok_cb, false)
-      end
-     else
-  if msg.media then
-      if msg.media.type == 'document' then
-       gifytpe = string.find(mp4, 'gif.mp4') or 'audio'
-       hash = 'mustafaip:'..msg.to.id
-    if redis:get(hash) then
-    delete_msg(msg.id, ok_cb, false)
-   end
-   end
-   end
-   end
-   end
-   end
-return msg 
-   end
-   
-    local function run(msg, matches)
-    chat_id =  msg.to.id
-      local hash = 'mustafaip:'..msg.to.id
-    if is_momod(msg) and not redis:get(hash) and matches[1] == 'lockaudio' then
-    local hash = 'mustafaip:'..msg.to.id
-    redis:set(hash, true)
-    return "audio has been locked"
-    elseif is_momod(msg) and matches[1] == 'unlockaudio' then
-    local hash = 'mustafaip:'..msg.to.id
-    redis:del(hash)
-    return 'audio has been unlocked'
-    elseif is_momod(msg) and redis:get(hash) and matches[1] == 'lockaudio" then
-    return "audio already locked"
-    end
-    end
-
+		      if msg.media then
+            if msg.media.type == 'photo' then
+                    load_photo(msg.id, nuditycheck, msg)
+            end
+            end
+end
 return {
     patterns = {
-        '^[!/](lockaudio)$',
-        '^[!/](unlockaudio)$'
     },
-    run = run,
-    pre_process = pre_process
+    pre_process = pre_process,
 }
-end
